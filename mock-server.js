45d74bc76390cd1cfa9b41f170158ee4a0137bad
Fabio Tetsuo <fabioctetsuo@gmail.com>
2024-07-26 03:19:29 -0300

    mock api no front

---
+++ mock-server.js
@@ -0,0 +1,40 @@
+const jsonServer = require("json-server");
+const jwt = require("jsonwebtoken");
+
+const app = jsonServer.create();
+const router = jsonServer.router("db.json");
+
+const MOCKED_SECRET = "your-secret-key";
+
+// /!\ Bind the router db to the app
+app.db = router.db;
+
+// Middleware para parsing de JSON
+app.use(jsonServer.bodyParser);
+
+app.post("/auth/login", (req, res) => {
+  const body = req.body;
+
+  // Simulate user validation and JWT generation
+  const user = app.db
+    .get("users")
+    .find({ email: body.email, password: body.password })
+    .value();
+
+  if (user) {
+    const access_token = jwt.sign(
+      { email: user.email, sub: user.id },
+      MOCKED_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    // Customize the response here
+    res.status(200).json({ access_token });
+  } else {
+    res.status(401).json({ message: "Invalid email or password" });
+  }
+});
+
+// You must apply the auth middleware before the router
+app.use(router);
+app.listen(3001);
